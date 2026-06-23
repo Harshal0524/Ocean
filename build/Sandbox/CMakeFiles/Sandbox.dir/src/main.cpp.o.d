0a851930aa817b7ce51452de9704151ba8edeac7
Harshal <Harshalhiren@gmail.com>
2026-06-23 19:36:22 -0400

    added CMake toold build option

--- build/Sandbox/CMakeFiles/Sandbox.dir/src/main.cpp.o.d
+++
@@ -1,4 +0,0 @@
-Sandbox/CMakeFiles/Sandbox.dir/src/main.cpp.o: \
- /home/Neo/GameEngine/Ocean/Sandbox/src/main.cpp \
- /usr/include/stdc-predef.h \
- /home/Neo/GameEngine/Ocean/Engine/include/Ocean/Application.h
